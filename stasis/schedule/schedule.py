788545f16a823783cc652ee01b05f2d7599ab8ce
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-06-28 15:35:01 -0700

    working on error handling

--- stasis/schedule/schedule.py
+++ stasis/schedule/schedule.py
@@ -19,47 +19,53 @@ def schedule(event, context):
     :return:
     """
 
-    body = json.loads(event['body'])
+    try:
+        body = json.loads(event['body'])
 
-    print(body)
+        print(body)
 
-    validate(body, __SCHEDULE__)
+        validate(body, __SCHEDULE__)
 
-    import boto3
-    overrides = {"containerOverrides": [{
-        "name": "carrot-runner",
-        "environment": [
-            {
-                "name": "SPRING_PROFILES_ACTIVE",
-                "value": "{},{}".format(body['env'], body['mode'])
+        import boto3
+        overrides = {"containerOverrides": [{
+            "name": "carrot-runner",
+            "environment": [
+                {
+                    "name": "SPRING_PROFILES_ACTIVE",
+                    "value": "{},{}".format(body['env'], body['mode'])
+                },
+                {
+                    "name": "CARROT_SAMPLE",
+                    "value": "{}".format(body['sample'])
+                },
+                {
+                    "name": "CARROT_METHOD",
+                    "value": "{}".format(body['method'])
+                }
+            ]
+        }]}
+
+        # fire AWS fargate instance now
+        client = boto3.client('ecs')
+        response = client.run_task(
+            cluster='carrot',  # name of the cluster
+            launchType='FARGATE',
+            taskDefinition='carrot-runner:1',
+            count=1,
+            platformVersion='LATEST',
+            networkConfiguration={
+                'awsvpcConfiguration': {
+                    'subnets': [
+                        # we need at least 2, to insure network stability
+                        os.environ.get('SUBNET', 'subnet-064fbf05a666c6557')],
+                    'assignPublicIp': 'ENABLED'
+                }
             },
-            {
-                "name": "CARROT_SAMPLE",
-                "value": "{}".format(body['sample'])
-            },
-            {
-                "name": "CARROT_METHOD",
-                "value": "{}".format(body['method'])
-            }
-        ]
-    }]}
+            overrides=overrides,
+        )
 
-    # fire AWS fargate instance now
-    client = boto3.client('ecs')
-    response = client.run_task(
-        cluster='carrot',  # name of the cluster
-        launchType='FARGATE',
-        taskDefinition='carrot-runner:1',
-        count=1,
-        platformVersion='LATEST',
-        networkConfiguration={
-            'awsvpcConfiguration': {
-                'subnets': [
-                    # we need at least 2, to insure network stability
-                    os.environ.get('SUBNET', 'subnet-064fbf05a666c6557')],
-                'assignPublicIp': 'ENABLED'
-            }
-        },
-        overrides=overrides,
-    )
-    return response
+        # fire status update to track sample is in scheduling
+        return response
+
+    except Error:
+        return
