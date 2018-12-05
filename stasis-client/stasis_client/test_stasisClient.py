98eda1eeee5e3940df89feea9e5d4998363b5811
Gert <berlinguyinca@gmail.com>
2018-12-05 11:11:02 -0800

    added stasis client test structure

---
+++ stasis-client/stasis_client/test_stasisClient.py
@@ -0,0 +1,28 @@
+from stasis_client.client import StasisClient
+
+url = ""
+key = ""
+
+client = StasisClient(url,key)
+
+def test_sample_state():
+    client.sample_state()
+
+def test_sample_result(self):
+    self.fail()
+
+
+def test_sample_schedule(self):
+    self.fail()
+
+
+def test_sample_exist(self):
+    self.fail()
+
+
+def test_sample_metadata(self):
+    self.fail()
+
+
+def test_experiment_samples(self):
+    self.fail()
