47d6e2b9409974bc738d6664c98970d92d65ba35
Diego Pedrosa <linuxmant@gmail.com>
2019-08-14 09:51:14 -0700

    Update latest ECS task definition revision number

--- stasis-server/stasis/schedule/schedule.py
+++ stasis-server/stasis/schedule/schedule.py
@@ -87,7 +87,7 @@ def secure_schedule(event, context):
     body['secured'] = True
 
     if 'task_version' not in body:
-        body['task_version'] = 160
+        body['task_version'] = 163
 
     # get topic refrence
     import boto3
