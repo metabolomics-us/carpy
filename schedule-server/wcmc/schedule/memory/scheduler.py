aa5f5d434064f861746bd9d7f50dcc8bf65e77c3
berlinguyinca <berlinguyinca@gmail.com>
2019-11-19 22:33:59 +0000

    improvement of the api

---
+++ schedule-server/wcmc/schedule/memory/scheduler.py
@@ -0,0 +1,60 @@
+from wcmc.schedule.scheduler import Scheduler, Job, SampleStateService, JobState, JobStore, SampleState, JobExecutor
+
+
+class MemoryScheduler(Scheduler):
+    """
+    non production memory scheduler
+    """
+
+    def set_job_state(self, id: str, state: JobState):
+        self.store[id] = state
+
+    def job_state(self, id: str) -> JobState:
+        return self.store.get(id, JobState.NOT_FOUND)
+
+    def __init__(self, executor: JobExecutor):
+        super().__init__(executor)
+        self.store = {}
+
+    def _get_state_service(self) -> SampleStateService:
+        return MemorySampleStateService()
+
+    def job_cancel(self, id: str):
+        self.store[id] = JobState.CANCELLED
+
+
+class MemoryJobStore(JobStore):
+    """
+    simple in memory job store
+    """
+
+    def __init__(self):
+        self.jobs = {}
+
+    def store(self, job: Job):
+        self.jobs[job.generate_id()] = job
+
+    def delete(self, job_id: str):
+        self.jobs[job_id] = None
+
+    def load(self, job_id: str) -> Job:
+        return self.jobs[job_id]
+
+
+class MemorySampleStateService(SampleStateService):
+    """
+    in memory implementation of the state service. This should not be used for production service obviously
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.keeper = {}
+
+    def _get_job_store(self) -> JobStore:
+        return MemoryJobStore()
+
+    def set_state(self, id: str, sample_name: str, state: SampleState):
+        self.keeper["{}_{}".format(id, sample_name)] = state
+
+    def _state_sample(self, id: str, sample_name: str) -> SampleState:
+        return self.keeper["{}_{}".format(id, sample_name)]
--- schedule-server/wcmc/schedule/scheduler.py
+++ schedule-server/wcmc/schedule/scheduler.py
@@ -24,6 +24,35 @@ class Job(NamedTuple):
         """
 
 
+class JobStore:
+    """
+    stores the job and its associated information in the backend of the system
+    """
+
+    @abstractmethod
+    def store(self, job: Job):
+        """
+        :param job:
+        :return:
+        """
+
+    @abstractmethod
+    def delete(self, job_id: str):
+        """
+
+        :param job_id:
+        :return:
+        """
+
+    @abstractmethod
+    def load(self, job_id: str) -> Job:
+        """
+
+        :param job_id:
+        :return:
+        """
+
+
 class SampleState(Enum):
     """
     state of a given sample
@@ -35,10 +64,38 @@ class SampleState(Enum):
     AGGREGATED = "aggregated"
 
 
+class JobState(Enum):
+    SCHEDULED = "scheduled"
+    RUNNING = "running"
+    DONE = "done"
+    CANCELLED = "cancelled"
+    NOT_FOUND = "not found"
+
+
 class SampleStateService:
     """
     utilized to process the state of a sample in the system
     """
+
+    def __init__(self):
+        self.job_store = self._get_job_store()
+
+    @abstractmethod
+    def _get_job_store(self) -> JobStore:
+        """
+        associated job store for this service
+        :return:
+        """
+
+    @abstractmethod
+    def set_state(self, id: str, sample_name: str, state: SampleState):
+        """
+        sets the state of a given sample for a given job in the system
+        :param id:
+        :param sample_name:
+        :param state:
+        :return:
+        """
 
     def _state(self, id: str) -> List[SampleState]:
         """
@@ -50,13 +107,13 @@ class SampleStateService:
         samples = self._load_samples(id)
         return list(map(lambda sample: self._state_sample(id, sample), samples))
 
-    @abstractmethod
     def _load_samples(self, id: str) -> List[str]:
         """
         loads all the samples associated with this job id
         :param id:
         :return:
         """
+        return self.job_store.load(id).samples
 
     @abstractmethod
     def _state_sample(self, id: str, sample_name: str) -> SampleState:
@@ -97,22 +154,61 @@ class SampleStateService:
         return self._is_state_done(id, SampleState.AGGREGATED)
 
 
+class JobExecutor:
+    """
+    actually executes a job or forwards it to a queue or whatever
+    """
+
+    def __init__(self):
+        self._scheduler = None
+
+    @property
+    def scheduler(self):
+        return self._scheduler
+
+    @scheduler.setter
+    def scheduler(self, value):
+        assert value is not None
+        self._scheduler = value
+
+    @abstractmethod
+    def execute(self, job: Job):
+        """
+
+        :param job:
+        :return:
+        """
+        pass
+
+
 class Scheduler:
     """
     a simple scheduler to process a list of files
     """
 
-    def __init__(self, state_service: SampleStateService):
+    def __init__(self, executor: JobExecutor):
         """
         :param state_service:
         """
-        self.state_service = state_service
+        self.state_service = self._get_state_service()
+        self.executor = executor
+        executor.scheduler = self
 
-    @abstractmethod
     def submit(self, job: Job):
         """
         submits a job to the quere
         :param job:
+        :return:
+        """
+        self.set_job_state(job.generate_id(), JobState.SCHEDULED)
+        self.executor.execute(job)
+
+    @abstractmethod
+    def set_job_state(self, id: str, state: JobState):
+        """
+        used to keep track of the job state
+        :param id:
+        :param state:
         :return:
         """
 
@@ -147,3 +243,17 @@ class Scheduler:
         :return:
         """
         return self.state_service.processing_done(id)
+
+    @abstractmethod
+    def job_state(self, id: str) -> JobState:
+        """
+        current state of the job
+        :return:
+        """
+
+    @abstractmethod
+    def _get_state_service(self) -> SampleStateService:
+        """
+        returns the associated sample state service
+        :return:
+        """
