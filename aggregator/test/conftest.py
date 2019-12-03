ded2c0fcb80fe20571a2a84515e201b29a19f8c6
berlinguyinca <berlinguyinca@gmail.com>
2019-12-03 20:27:33 +0000

    always set test variables

--- aggregator/test/conftest.py
+++ aggregator/test/conftest.py
@@ -1,8 +1,4 @@
-# Set AWS environment variables if they don't exist before importing moto/boto3
 import os
 
-if 'AWS_DEFAULT_REGION' not in os.environ:
-    os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
-
-if 'PROD_STASIS_API_TOKEN' not in os.environ:
-    os.environ['PROD_STASIS_API_TOKEN'] = 'GUec9mh1jc6VFbudSzxfz8aIqdRiadqw6wWBzRCB'
+os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
+os.environ['PROD_STASIS_API_TOKEN'] = 'GUec9mh1jc6VFbudSzxfz8aIqdRiadqw6wWBzRCB'
