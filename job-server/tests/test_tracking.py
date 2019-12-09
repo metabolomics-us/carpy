8853f66608b5a03c93a5e3ea5e10818fb525b960
berlinguyinca <berlinguyinca@gmail.com>
2019-12-09 19:38:32 +0000

    fixed testing impl

--- job-server/tests/test_tracking.py
+++ job-server/tests/test_tracking.py
@@ -33,4 +33,34 @@ def test_create_and_get(requireMocking):
 
     # ensure status is updated now
 
-    assert False
+    assert 'job' in result
+    assert 'sample' in result
+    assert 'state' in result
+    assert 'id' in result
+
+    assert result['id'] == '12345_abc'
+    assert result['state'] == 'scheduled'
+
+    # check if update works correctly
+    data = json.dumps({
+        "job": "12345",
+        "sample": "abc",
+        "state": "processed"
+    })
+
+    result = tracking.create({'body': data}, {})
+
+    result = tracking.get({
+        "pathParameters": {
+            "sample": "abc",
+            "job": "12345"
+        }
+    }, {})
+
+    assert result is not None
+
+    assert result['statusCode'] == 200
+    assert 'body' in result
+    result = json.loads(result['body'])
+
+    assert result['state'] == 'processed'
