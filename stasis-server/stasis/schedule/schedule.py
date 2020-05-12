737446bb2eb7d3693e93112d9ed67ce5afff7dbb
berlinguyinca <berlinguyinca@gmail.com>
2020-05-12 09:52:06 -0700

    fixe dminor profile bug

--- stasis-server/stasis/schedule/schedule.py
+++ stasis-server/stasis/schedule/schedule.py
@@ -275,7 +275,7 @@ def schedule_processing_to_fargate(event, context):
             "environment": [
                 {
                     "name": "SPRING_PROFILES_ACTIVE",
-                    "value": "{}-{},{}".format('aws',body['env'], body['profile'])
+                    "value": "{}{},{}".format('aws',body['env'], body['profile'])
                     # AWS profile needs to be active for this system to connect to the AWS database
                 },
                 {
