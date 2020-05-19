8c775666ca29d5316b76256e37bb7d4efff48926
berlinguyinca <berlinguyinca@gmail.com>
2020-05-19 10:11:00 -0700

    working on test layouting

---
+++ cis-server/tests/conftest.py
@@ -0,0 +1,30 @@
+# Set AWS environment variables if they don't exist before importing moto/boto3
+import os
+
+import moto
+import pytest
+
+os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
+
+
+@pytest.fixture
+def requireMocking():
+    """
+    method which should be called before all other methods in tests. It basically configures our
+    mocking context for stasis
+    """
+
+    lamb = moto.mock_lambda()
+    lamb.start()
+
+    os.environ["current_stage"] = "test"
+    os.environ["carrot_database"] = "carrot-test"
+    os.environ["carrot_username"] = "postgres"
+    os.environ["carrot_password"] = "Fiehnlab2020"
+    os.environ["carrot_url"] = "lc-binbase-dev.czbqhgrlaqbf.us-west-2.rds.amazonaws.com"
+    os.environ["carrot_port"] = "5432"
+
+    yield
+    lamb.stop()
+
+    pass
