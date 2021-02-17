555866597caffd1d778183fa18e1560373ae680e
berlinguyinca <berlinguyinca@gmail.com>
2021-02-17 15:00:40 -0800

    Added build script

---
+++ stasis-server/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+##
+# very simple deployment script to upload all our stacks
+#
+export SLS_DEBUG=*
+
+sls create_domain --config serverless-schedule.yml --stage $1
+sls create_domain --config serverless-stasis.yml --stage $1
+#
+#sls remove --config serverless-schedule.yml --stage $1
+#sls remove --config serverless-stasis.yml --stage $1
+#sls remove --config serverless-resources.yml --stage $1
+
+sls deploy --config serverless-resources.yml --stage $1
+sls deploy --config serverless-stasis.yml --stage $1
+sls deploy --config serverless-schedule.yml --stage $1
+
+
+
+
